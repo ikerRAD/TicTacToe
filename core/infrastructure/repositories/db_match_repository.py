63ae9b9a47cf8943bb7e80b20e79b3a42ab0653d
IkerRAD <ikerbueno30@gmail.com>
2023-08-27 18:34:11 +0200

    feat(MatchRepository): add method to find user by id and fix the bug of not counting victories

--- core/infrastructure/repositories/db_match_repository.py
+++ core/infrastructure/repositories/db_match_repository.py
@@ -1,10 +1,12 @@
 from typing import Optional
+from django.db.models import Q
+
 from core.domain.exceptions.match_not_accepting_guests_exception import (
     MatchNotAcceptingGuestsException,
 )
 from core.domain.exceptions.match_not_found_exception import MatchNotFoundException
 from core.domain.exceptions.repeated_player_exception import RepeatedPlayerException
-from core.domain.models.match import Match, MatchStatuses
+from core.domain.models.match import Match, MatchStatuses, MatchPlayer
 from core.domain.models.user import User
 from core.domain.repositories.match_repository import MatchRepository
 
@@ -14,6 +16,12 @@ class DbMatchRepository(MatchRepository):
 
     def __init__(self):
         self.__match_manager = Match.objects
+
+    def find_by_user_id(self, user_id: int) -> list[Match]:
+        first_player_query = Q(first_player_id=user_id)
+        second_player_query = Q(second_player_id=user_id)
+
+        return self.__match_manager.filter(first_player_query | second_player_query)
 
     def find_or_fail_by_id(self, match_id: int) -> Match:
         try:
@@ -58,3 +66,13 @@ class DbMatchRepository(MatchRepository):
         match.status = MatchStatuses.FINISHED
         match.set_which_player_wins(winner)
         match.save()
+
+        if winner is not None:
+            if winner == MatchPlayer.FIRST_PLAYER.value:
+                winner_player = match.first_player
+                winner_player.matches_won = winner_player.matches_won + 1
+                winner_player.save()
+            elif winner == MatchPlayer.SECOND_PLAYER.value:
+                winner_player = match.second_player
+                winner_player.matches_won = winner_player.matches_won + 1
+                winner_player.save()
